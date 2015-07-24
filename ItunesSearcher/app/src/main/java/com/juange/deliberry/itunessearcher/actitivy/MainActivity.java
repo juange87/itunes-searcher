661a63a19832099be20260e99a3cbc0f4f8f8b7d
Juan Jesus Fernandez Madrid <juangefm@gmail.com>
2015-07-24 03:41:45 +0200

    collapse search view when searching

--- ItunesSearcher/app/src/main/java/com/juange/deliberry/itunessearcher/actitivy/MainActivity.java
+++ ItunesSearcher/app/src/main/java/com/juange/deliberry/itunessearcher/actitivy/MainActivity.java
@@ -10,20 +10,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.FrameLayout;
-import android.widget.Toast;
-import butterknife.Bind;
+import android.view.MenuItem;
 import butterknife.ButterKnife;
 import com.juange.deliberry.itunessearcher.R;
 import com.juange.deliberry.itunessearcher.fragment.MainFragment;
 import com.juange.deliberry.itunessearcher.fragment.ResultsFragment;
 
-public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
-
-    @Bind(R.id.fragment_container)
-    FrameLayout fragmentContainer;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,32 +33,27 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         inflater.inflate(R.menu.menu_search, menu);
 
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        final MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) item.getActionView();
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchView.setIconifiedByDefault(false);
-        searchView.setOnQueryTextListener(this);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                item.collapseActionView();
+
+                replaceFragment(ResultsFragment.newInstance(query));
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
         return true;
-    }
-
-    @Override
-    public boolean onQueryTextSubmit(String query) {
-        View view = this.getCurrentFocus();
-        if (view != null) {
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
-
-        Toast.makeText(this, query, Toast.LENGTH_LONG).show();
-
-        replaceFragment(ResultsFragment.newInstance(query));
-
-        return true;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String newText) {
-        return false;
     }
 
     public void replaceFragment(Fragment fragment) {
