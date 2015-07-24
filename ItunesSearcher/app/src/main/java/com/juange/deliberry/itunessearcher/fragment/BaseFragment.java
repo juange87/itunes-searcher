e29e161f553d06b3fbf0501851bec868e2e0238a
Juan Jesus Fernandez Madrid <juangefm@gmail.com>
2015-07-24 03:40:27 +0200

    add destroy subject to avoid subscribing request queen fragment is destroyed

--- ItunesSearcher/app/src/main/java/com/juange/deliberry/itunessearcher/fragment/BaseFragment.java
+++ ItunesSearcher/app/src/main/java/com/juange/deliberry/itunessearcher/fragment/BaseFragment.java
@@ -2,16 +2,28 @@ package com.juange.deliberry.itunessearcher.fragment;
 
 import android.support.v4.app.Fragment;
 import com.juange.deliberry.itunessearcher.actitivy.MainActivity;
+import rx.Observable;
+import rx.subjects.PublishSubject;
 
 /**
  * Created by juange on 23/7/15.
  */
 public class BaseFragment extends Fragment {
 
+    PublishSubject<Void> destroyedSubject = PublishSubject.create();
+
     public MainActivity getMainActivity() {
         if (getActivity() instanceof MainActivity) {
             return ((MainActivity) getActivity());
         }
         return null;
+    }
+
+    public Observable<Void> destroyed() { return destroyedSubject.asObservable(); }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        destroyedSubject.onNext(null);
     }
 }
